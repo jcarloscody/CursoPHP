a2e2923a33808b2dd970938f39ca87d86bbe4cb8
Josue <josuecarlosos2@gmail.com>
2021-09-08 17:28:40 -0300

    aprendendo sobre update, insert, delete...

---
+++ s13_BancoDados-DAO_PDO/LIST/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("config.php");
+
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
+
+?>
